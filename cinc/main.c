14a762bfe6ae72c751e0ce23757eda9524a03c1c
pjht <pjht@users.noreply.github.com>
2018-09-09 20:12:20 -0500

    out.s now out.mys and debug added

--- cinc/main.c
+++ cinc/main.c
@@ -14,14 +14,36 @@
 #include "parser.h"
 #include "generate.h"
 
+#define PARSER_DEBUG 0
+
 int main(int argc, const char * argv[]) {
-    char* prgstr="int main() {\nreturn 0;\n}";
+    char* prgstr="int main() {\n  return 173;\n}\n";
+    if (PARSER_DEBUG) {
+        printf("Program:\n");
+        printf("%s",prgstr);
+    }
     Token* tokens=tokenize(prgstr);
+    if (PARSER_DEBUG) {
+        printf("Tokens:\n");
+        Token* tok=tokens;
+        while (tok) {
+            print_tok(tok);
+            tok=tok->next;
+        }
+    }
     AstNode* ast=parse(tokens);
     free_toklist(tokens);
+    if (PARSER_DEBUG) {
+        printf("AST:\n");
+        print_tree(ast, 0);
+    }
     char* prg=generate_prg(ast);
     free_tree(ast);
-    FILE* outfile=fopen("/Users/peterterpstra/Desktop/projects/xcode/cinc/cinc/out.s","w");
+    if (PARSER_DEBUG) {
+        printf("Output assembly:\n");
+        printf("%s",prg);
+    }
+    FILE* outfile=fopen("/Users/peterterpstra/Desktop/projects/xcode/cinc/cinc/out.mys","w");
     fputs(prg, outfile);
     fclose(outfile);
     free(prg);
